ae2c24ee269b62264d77fb224e44dfe443f73d50
leojay-net <102752992+leojay-net@users.noreply.github.com>
2023-09-08 20:22:50 +0100

    Update dfs3.py

--- dfs3.py
+++ dfs3.py
@@ -1,17 +1,69 @@
-# Queue is a data structure that operates on a FIFO cycle(First In First Out)
-from queue import Queue  
-
-
-# copy has two methods to create copies
-# deepcopy() is used to create a copy of the queue without any connection to the parent queue
-# copy() is used to to create a copy of the queue with connection to the parent queue
 import copy
-
-# time is used to calculate the time spent solving the puzzle
 import time
 
+def neighbours(p_state):
+    nexts = []
+    empty_tile = p_state.index(0)
+    currentNode = p_state
+    if empty_tile!=0 and empty_tile!=1 and empty_tile!=2: # check if the empty file is not on the first row of the puzzle
+        State = copy.deepcopy(currentNode) # creates a copy of currentnode and is not connected to the currentnode
+        State[empty_tile] = State[empty_tile-3] # set the location place of zero to another value 
+        State[empty_tile-3] = 0 # set the location of another value to 0
+        nexts.append(State)
+    if empty_tile!=0 and empty_tile!=3 and empty_tile!=6: # checks if the empty_tile is not on the first column to move it to the left.
+        State = copy.deepcopy(currentNode)
+        State[empty_tile] = State[empty_tile-1]
+        State[empty_tile-1] = 0
+        nexts.append(State)
+    if empty_tile!=6 and empty_tile!=7 and empty_tile!=8: # checks if the empty_tile is not on the last row column to move it down.
+        State = copy.deepcopy(currentNode)
+        State[empty_tile] = State[empty_tile+3]
+        State[empty_tile+3] = 0
+        nexts.append(State)
+    if empty_tile!=2 and empty_tile!=5 and empty_tile!=8: # checks if the empty_tile is not on the last column to move it to the right.
+        State = copy.deepcopy(currentNode)
+        State[empty_tile] = State[empty_tile+1]
+        State[empty_tile+1] = 0
+        nexts.append(State)
+    return nexts
 
-# function to display puzzle in the matrix format 
+def is_goal_state(state):
+
+    return state == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def solve_8puzzle(initial_board):
+  # Create a stack to store the board configurations that we need to visit
+  global step
+  step = 0
+  stack = []
+  # Push the initial board configuration onto the stack
+  stack.append(initial_board)
+  # Create a set to store the board configurations that we have already visited
+  #print("stack=",stack)
+  visited_boards = set()
+  # While the stack is not empty
+  while not len(stack) == 0:
+    # Pop the top board configuration from the stack
+    board = stack.pop()
+    # If the board is the goal state, return it
+    if is_goal_state(board):
+      return display(board)
+    #print(1)
+    # Otherwise, add the board to the set of visited board configurations
+    visited_boards.add(tuple(board))
+    #print("visited = ",visited_boards)
+    # Generate the list of possible next board configurations
+    next_boards = neighbours(board)
+    # For each possible next board configuration
+    for next_board in next_boards:
+      # If the board has not been visited
+      if tuple(next_board) not in visited_boards:
+        # Push it onto the stack
+        stack.append(next_board)
+    
+  # If we have exhausted the stack and not found the goal state, return None
+  return None
 def display(p):
     global step # makes the step global.....to be used anywhere in the program
     for i in range(len(p)):
@@ -19,61 +71,10 @@ def display(p):
             print("")
         print(str(p[i])+" ",end="")
     print("\n")
-    print("steps :",step)
+    #print("steps :",step)
     step += 1
-    
-# function to check the puzzle and also change the current node
-def solve_puzzle(puzzle_state):
-    global puzzle_index
-    if puzzle_state==final_puzzle: # check if the current puzzle is equal to the final puzzle
-        display(puzzle_state) # takes the puzzle to the function display and calls it
-        return True # return true if they are equal
-    if puzzle_state not in visitedList: # then we check if we have worked with that particular puzzle before so we don't end up in an endless loop
-        display(puzzle_state)  # takes the puzzle to the function display and calls it
-        queue.insert(puzzle_index, puzzle_state) # inserting the puzzle in a specific index
-        visitedList.append(puzzle_state) # append puzzle to visitedlist so we don't repeat the same puzzle_state again
-        puzzle_index +=1
-    return False
 
-
-initial_puzzle = [1,0,6, 3,4,5, 2,7,8]
-final_puzzle = [0,1,2, 3,4,5, 6,7,8]
-
-found = False
-step = 0
-visitedList = []
-queue = []
-queue.append(initial_puzzle) # moves the initial puzzle state into the queue
-visitedList.append(initial_puzzle) # moves the initial puzzle state into the list 
-
-t0 = time.time() # initial time or start  time
-
-while (not found and not len(queue)==0): # check for if found is set to false and the queue is empty
-    currentNode = queue.pop(0) # pop out the first value from the list
-    empty_tile = currentNode.index(0) # set to the index value of 0 which stands for empty file
-    puzzle_index = 0
-
-    if empty_tile!=0 and empty_tile!=1 and empty_tile!=2: # check if the empty file is not on the first row of the puzzle
-        State = copy.deepcopy(currentNode) # creates a copy of currentnode and is not connected to the currentnode
-        State[empty_tile] = State[empty_tile-3] # set the location place of zero to another value 
-        State[empty_tile-3] = 0 # set the location of another value to 0
-        found = solve_puzzle(State) # call function solve_puzzle()
-    if empty_tile!=0 and empty_tile!=3 and empty_tile!=6 and found==False: # checks if the empty_tile is not on the first column to move it to the left.
-        State = copy.deepcopy(currentNode)
-        State[empty_tile] = State[empty_tile-1]
-        State[empty_tile-1] = 0
-        found = solve_puzzle(State)
-    if empty_tile!=6 and empty_tile!=7 and empty_tile!=8 and found==False: # checks if the empty_tile is not on the last row column to move it down.
-        State = copy.deepcopy(currentNode)
-        State[empty_tile] = State[empty_tile+3]
-        State[empty_tile+3] = 0
-        found = solve_puzzle(State)
-    if empty_tile!=2 and empty_tile!=5 and empty_tile!=8 and found==False: # checks if the empty_tile is not on the last column to move it to the right.
-        State = copy.deepcopy(currentNode)
-        State[empty_tile] = State[empty_tile+1]
-        State[empty_tile+1] = 0
-        found = solve_puzzle(State)
-t1 = time.time() # subtracts final time with initial time to get the time it takes to solve the puzzle
-print('------')
-print('Time:', t1-t0)
-
+t0 = time.time()
+print(solve_8puzzle([1, 2, 5, 3, 4, 8, 6, 7, 0]))
+t1 = time.time()
+print("time=", t1-t0)
